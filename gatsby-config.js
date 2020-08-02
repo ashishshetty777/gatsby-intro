cd21cf550d5a07d84b8e2a91157c8b229f4036bf
Ashish Shetty <ashishshetty777@gmail.com>
2020-08-02 16:02:34 +0530

    Site metadata and GQL query added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Gatsby Workshop',
+    description:
+      'A site that I built during a full day Frontend masters workshop',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
