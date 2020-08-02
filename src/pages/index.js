a8646d925972ab81fa75dbb72bb440ec47466963
Ashish Shetty <ashishshetty777@gmail.com>
2020-08-02 14:54:10 +0530

    Gatsby basics

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>Home</h1>
+    <p> Hello Mumbai!</p>
+    <Link to="/about/"> Learn about me </Link>
+  </>
+);
