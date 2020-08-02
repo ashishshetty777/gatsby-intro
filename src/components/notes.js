e6953c98c6f9c9c0848c39e98fcfbc13904210f6
Ashish Shetty <ashishshetty777@gmail.com>
2020-08-02 21:17:20 +0530

    Handling images in gastby added

--- src/components/notes.js
+++ src/components/notes.js
@@ -10,3 +10,26 @@
 //       }
 //     }
 //   }
+
+// Gatsby Benifits
+
+// Follows the PRPL pattern and other performance best practices
+// Generates only static assets (no server required)
+// Optimizes and lazy-loads assets
+// Normalizes third-party data
+
+// Image Sharp Transformer
+
+// query {
+//     allFile(filter: {sourceInstanceName: {eq: "images"}}){
+//       nodes {
+//         relativePath
+//         childImageSharp {
+//           fluid {
+//             src
+//             srcSet
+//           }
+//         }
+//       }
+//     }
+//   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,19 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p> Hello Mumbai!</p>
-      <Link to="/about/"> Learn about me </Link>
-      <h2> Read my blogs</h2>
-      {posts.map(post => {
-        return <PostPreview key={post.slug} post={post} />;
-      })}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2> Read my blogs</h2>
+        {posts.map(post => {
+          return <PostPreview key={post.slug} post={post} />;
+        })}
+      </Layout>
+    </>
   );
 };
